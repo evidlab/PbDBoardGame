27809e9d11e0329e3592fe1a255fa924161aec5e
Reggie Padgett <reggie.padgett2@gmail.com>
2018-10-22 17:44:50 -0400

    deleted unused paths

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,8 @@ var passport = require("passport");
 //Auth Routes
 //====================
 
+
+
 router.get("/", function(req, res){
   res.render("landing");
 });
@@ -23,44 +25,44 @@ router.get("/gameBoard", function(req, res){
   res.render("gameBoard/index");
 });
 
-//handle sign up logic
-router.post("/register", function(req, res){
-  var newUser = new User({
-    username: req.body.username,
-    firstName: req.body.firstName,
-    LastName: req.body.LastName,
-    email: req.body.email,
-    avatar: req.body.avatar
-  });
-
-  User.register(newUser, req.body.password, function(err, user){
-          if(err){
-              console.log(err);
-              req.flash("error", err.message);
-              return res.redirect('/register');
-          }
-          passport.authenticate("local")(req, res, function(){
-             req.flash("success", "Successfully Signed Up! Nice to meet you " + req.body.username);
-             res.redirect("/campgrounds");
-          });
-      });
-  });
-
-//Handling Login logic
-router.post("/login", passport.authenticate("local",
-  {
-    successRedirect: "/campgrounds",
-    failureRedirect: "/login"
-  }), function(req, res){
-
-});
-
-//Logout logic
-router.get("/logout", function(req, res){
-  req.logout();
-  req.flash("success", "Logged out succesfully!");
-  res.redirect("/campgrounds");
-});
+// //handle sign up logic
+// router.post("/register", function(req, res){
+//   var newUser = new User({
+//     username: req.body.username,
+//     firstName: req.body.firstName,
+//     LastName: req.body.LastName,
+//     email: req.body.email,
+//     avatar: req.body.avatar
+//   });
+//
+//   User.register(newUser, req.body.password, function(err, user){
+//           if(err){
+//               console.log(err);
+//               req.flash("error", err.message);
+//               return res.redirect('/register');
+//           }
+//           passport.authenticate("local")(req, res, function(){
+//              req.flash("success", "Successfully Signed Up! Nice to meet you " + req.body.username);
+//              res.redirect("/campgrounds");
+//           });
+//       });
+//   });
+//
+// //Handling Login logic
+// router.post("/login", passport.authenticate("local",
+//   {
+//     successRedirect: "/campgrounds",
+//     failureRedirect: "/login"
+//   }), function(req, res){
+//
+// });
+//
+// //Logout logic
+// router.get("/logout", function(req, res){
+//   req.logout();
+//   req.flash("success", "Logged out succesfully!");
+//   res.redirect("/campgrounds");
+// });
 
 
 module.exports = router;
